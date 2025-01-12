6b9dbdc21e080a14c5e1db35f085bcb6e8c77368
Mike Aizatsky <mike.aizatsky@gmail.com>
2025-01-11 21:23:54 -0800

    pattern

--- Justfile
+++ Justfile
@@ -4,4 +4,6 @@ watch +WATCH_TARGET='run':
     watchexec -rc -w . --ignore *.results -- just {{WATCH_TARGET}}
 
 run:
-    cargo run -- --out target/out.midi
+    cargo run -- --bpm 300 --out target/clave23.mid --pattern "rrcrcrrrcrrcrrcr"
+    cargo run -- --bpm 300 --out target/cascara.mid  --pattern "c>rc>rcc>rcc>rcc>rcrc"
+    cargo run -- --bpm 300 --out target/clave32.mid --pattern "crrcrrcrrrcrcrrr"
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use core::panic;
 use std::{fs::File, io::BufWriter, path::PathBuf};
 
 use clap::Parser;
 use midly::{Header, TrackEvent};
+use regex::Regex;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -9,49 +11,127 @@ struct Args {
     out: PathBuf,
 
     #[arg(short, long, default_value_t = 120)]
-    bpm: i16,
+    bpm: u16,
+
+    #[arg(short, long, default_value_t = String::from("m>mmm"))]
+    pattern: String,
+
+    #[arg(long, default_value_t = 10)]
+    channel: i8,
+
+    #[arg(long, default_value_t = 10_000)]
+    beats: usize,
+
+    #[arg(long, default_value_t = 63)]
+    vel: u8,
+
+    #[arg(long, default_value_t = 127)]
+    acc_vel: u8,
+
+    #[arg(long, default_value_t = 31)]
+    ghost_vel: u8,
 }
 
 fn main() {
     let args = Args::parse();
+    args.main();
+}
 
-    let ticks_per_beat = 96;
-    let header = Header {
-        format: midly::Format::SingleTrack,
-        timing: midly::Timing::Metrical(ticks_per_beat.into()),
-    };
+impl Args {
+    fn main(&self) {
+        assert!(self.channel > 0, "channel needs to be >0");
 
-    let channel = 9.into();
-    let key = 75.into();
+        let ticks_per_beat = 96;
+        let delta = 96u32;
+        let channel = 9.into();
 
-    let click_on = TrackEvent {
-        // todo: first one should be 0
-        delta: 96.into(),
-        kind: midly::TrackEventKind::Midi {
-            channel,
-            message: midly::MidiMessage::NoteOn {
-                key,
-                vel: 100.into(),
-            },
-        },
-    };
-    let end_of_track = TrackEvent {
-        delta: 96.into(),
-        kind: midly::TrackEventKind::Meta(midly::MetaMessage::EndOfTrack),
-    };
-    let mut track = vec![];
+        let tempo = 60.0 * 1_000_000.0 / (self.bpm as f64);
+        let tempo = tempo as u32;
 
-    for _ in 0..100 {
-        track.push(&click_on);
+        let bpm = 60.0 * 1_000_000.0 / (tempo as f64);
+        println!("effective bpm: {bpm}");
+
+        let header = Header {
+            format: midly::Format::SingleTrack,
+            timing: midly::Timing::Metrical(ticks_per_beat.into()),
+        };
+
+        let set_tempo = TrackEvent {
+            delta: 0.into(),
+            kind: midly::TrackEventKind::Meta(midly::MetaMessage::Tempo(tempo.into())),
+        };
+
+        let end_of_track = TrackEvent {
+            delta: delta.into(),
+            kind: midly::TrackEventKind::Meta(midly::MetaMessage::EndOfTrack),
+        };
+
+        let pattern = self.parse_pattern();
+
+        let mut events = vec![set_tempo];
+        let mut d = 0;
+        for i in 0..self.beats {
+            let item = &pattern[i % pattern.len()];
+            match item {
+                PatternItem::Rest => d += delta,
+                PatternItem::Note { key, vel } => {
+                    events.push(TrackEvent {
+                        delta: d.into(),
+                        kind: midly::TrackEventKind::Midi {
+                            channel,
+                            message: midly::MidiMessage::NoteOn {
+                                key: (*key).into(),
+                                vel: (*vel).into(),
+                            },
+                        },
+                    });
+                    d = delta;
+                }
+            };
+        }
+        events.push(end_of_track);
+
+        let mut track = Vec::with_capacity(events.len());
+        for e in events.iter() {
+            track.push(e)
+        }
+        let tracks = vec![track];
+
+        {
+            let file = File::create(&self.out).expect("error creating output file");
+            let mut writer = BufWriter::new(file);
+            midly::write_std(&header, tracks, &mut writer).expect("error writing file");
+        }
     }
-    track.push(&end_of_track);
 
-    let tracks = vec![track];
+    fn parse_pattern(&self) -> Vec<PatternItem> {
+        let re = Regex::new(r"[cmr][>]?").expect("bad regexp");
+        let mut events = vec![];
 
-    {
-        let file = File::create(&args.out).expect("error creating output file");
-        let mut writer = BufWriter::new(file);
-    
-        midly::write_std(&header, tracks, &mut writer).expect("error writing file");
+        for cap in re.captures_iter(&self.pattern) {
+            let cap = cap.get(0).unwrap().as_str();
+            if cap.starts_with("r") {
+                events.push(PatternItem::Rest);
+                continue;
+            }
+            let key = match cap.chars().next().unwrap() {
+                'm' => 32,
+                'c' => 75,
+                _ => unimplemented!("bad key: {cap:?}"),
+            };
+            let vel = if cap.ends_with(">") {
+                self.acc_vel
+            } else {
+                self.vel
+            };
+            events.push(PatternItem::Note { key, vel });
+        }
+        assert!(!events.is_empty(), "bad pattern: {}", self.pattern);
+        events
     }
+}
+
+enum PatternItem {
+    Rest,
+    Note { key: u8, vel: u8 },
 }
